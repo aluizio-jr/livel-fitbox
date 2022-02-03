2e57c55c3a7666c6bf22a0e0e1e39ec658c39686
aluizio-jr <aluizio.fg@gmail.com>
2022-02-03 20:39:42 -0300

    post2

--- venda_gravar.php
+++ venda_gravar.php
@@ -2,21 +2,21 @@
 
 function getPost()
 {
-    if(!empty($_POST))
-    {
-        // when using application/x-www-form-urlencoded or multipart/form-data as the HTTP Content-Type in the request
-        // NOTE: if this is the case and $_POST is empty, check the variables_order in php.ini! - it must contain the letter P
-        return $_POST;
-    }
-
-    // // when using application/json as the HTTP Content-Type in the request 
-    // $post = json_decode(file_get_contents('php://input'), true);
-    // if(json_last_error() == JSON_ERROR_NONE)
+    // if(!empty($_POST))
     // {
-    //     return $post;
+    //     // when using application/x-www-form-urlencoded or multipart/form-data as the HTTP Content-Type in the request
+    //     // NOTE: if this is the case and $_POST is empty, check the variables_order in php.ini! - it must contain the letter P
+    //     return $_POST;
     // }
 
-    // return [];
+    // when using application/json as the HTTP Content-Type in the request 
+    $post = json_decode(file_get_contents('php://input'), true);
+    if(json_last_error() == JSON_ERROR_NONE)
+    {
+        return $post;
+    }
+
+    return [];
 }
 
 print_r(getPost());
