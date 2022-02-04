4e3eef64b8f872f3c15f64c5d674decb99daddc9
aluizio-jr <aluizio.fg@gmail.com>
2022-02-04 08:58:18 -0300

    venda

--- venda_gravar.php
+++ venda_gravar.php
@@ -2,3 +2,7 @@
     require_once "classes/db_class.php"; 
     require_once "classes/functions.php";
     require_once "classes/post_data.php";
+
+    $venda_data = getPost();
+
+    print_r($venda_data);
