0a4115206240541bc2946784f40d11ace375259f
aluizio-jr <aluizio.fg@gmail.com>
2022-02-04 10:25:52 -0300

    venda

--- venda_gravar.php
+++ venda_gravar.php
@@ -8,6 +8,17 @@
 //    echo 'ID Aluno: ' . $vendaPost['id_aluno'];
 //    echo "\n";
 //    echo 'Data Venda: ' . $vendaPost['venda_data'];
+
+    $venda_count = count($vendaPost);
+    $itens_count = count($vendaPost['itens']);
+    $parcelas_count = count($vendaPost['parcelas']);
+
+    echo "Venda: " . $venda_count;
+    echo "\n";
+    echo "Itens: " . $itens_count;
+    echo "\n";
+    echo "Parcelas: " . $parcelas_count;
+
 function recursive_show_array($arr)
 {
     foreach($arr as $value)
