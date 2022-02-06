86acf8d9d11048430195f151dcf9bf0b7939a039
aluizio-jr <aluizio.fg@gmail.com>
2022-02-06 09:08:01 -0300

    vendas 3

--- venda_gravar.php
+++ venda_gravar.php
@@ -70,7 +70,7 @@
         try {
             $validaVenda = validaVenda($vendaData);
             if (!$validaVenda['validou']) throw new Exception($validaVenda['error']);
-            //return $vendaData['itens'];
+            return $vendaData['itens'];
 
             $validaItens = validaItens($vendaData['itens']);
             if (!$validaItens['validou']) throw new Exception($validaItens['error']);
