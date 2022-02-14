14bf2296600532029ee95936e3913f75150f4042
aluizio-jr <aluizio.fg@gmail.com>
2022-02-14 10:40:44 -0300

    asaas

--- classes/asaas/asaas_clientes.php
+++ classes/asaas/asaas_clientes.php
@@ -67,7 +67,31 @@
             curl_close($ch);
             echo $response;
             $retCliente = json_decode($response, true);
-            echo json_last_error();
+
+            switch (json_last_error()) {
+                case JSON_ERROR_NONE:
+                    echo ' - No errors';
+                break;
+                case JSON_ERROR_DEPTH:
+                    echo ' - Maximum stack depth exceeded';
+                break;
+                case JSON_ERROR_STATE_MISMATCH:
+                    echo ' - Underflow or the modes mismatch';
+                break;
+                case JSON_ERROR_CTRL_CHAR:
+                    echo ' - Unexpected control character found';
+                break;
+                case JSON_ERROR_SYNTAX:
+                    echo ' - Syntax error, malformed JSON';
+                break;
+                case JSON_ERROR_UTF8:
+                    echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+                break;
+                default:
+                    echo ' - Unknown error';
+                break;
+            }
+            
             var_dump($retCliente);
             //if (!is_array($retCliente)) throw new Exception("Nao foi possivel cadastrar o cliente: ASAAS_NO_RET"); 
             if (!$retCliente['ALUNO_CADASTRO']['id'])  throw new Exception("Nao foi possivel cadastrar o cliente: ASAAS_NO_ID"); 
