bfc4cb7a6400d0c235a1fcf08e66dc8a5d4cdda8
aluizio-jr <aluizio.fg@gmail.com>
2022-03-04 17:45:31 -0300

    gravar parcelas

--- venda_gravar.php
+++ venda_gravar.php
@@ -63,33 +63,11 @@
     $vendaPost = file_get_contents('php://input');
     $vendaPost = utf8_decode($vendaPost);
     $vendaPost = json_decode($vendaPost, true); //getPost();
-    switch (json_last_error()) {
-        case JSON_ERROR_NONE:
-            echo 'No errors';
-            break;
-        case JSON_ERROR_DEPTH:
-            echo 'Maximum stack depth exceeded';
-            break;
-        case JSON_ERROR_STATE_MISMATCH:
-            echo 'Underflow or the modes mismatch';
-            break;
-        case JSON_ERROR_CTRL_CHAR:
-            echo 'Unexpected control character found';
-            break;
-        case JSON_ERROR_SYNTAX:
-            echo 'Syntax error, malformed JSON';
-            break;
-        case JSON_ERROR_UTF8:
-            echo 'Malformed UTF-8 characters, possibly incorrectly encoded';
-            break;
-        default:
-            echo 'Unknown error';
-            break;
-    }    
 
+    print_r($vendaPost);
 
-    $retVenda = gravarVenda($vendaPost);
-    echo json_encode($retVenda, JSON_UNESCAPED_SLASHES|JSON_NUMERIC_CHECK);
+    // $retVenda = gravarVenda($vendaPost);
+    // echo json_encode($retVenda, JSON_UNESCAPED_SLASHES|JSON_NUMERIC_CHECK);
 
     
 
