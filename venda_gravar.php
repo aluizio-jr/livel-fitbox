59d54755f27d1177299e8deff38b159db689e663
aluizio-jr <aluizio.fg@gmail.com>
2022-03-19 20:46:39 -0300

    vb json

--- venda_gravar.php
+++ venda_gravar.php
@@ -63,34 +63,12 @@
 
     $vendaPost = file_get_contents('php://input');
     $vendaPost = utf8_decode($vendaPost);
-    $vendaPost = json_decode($vendaPost, true); //getPost();
-    switch (json_last_error()) {
-        case JSON_ERROR_NONE:
-            echo 'No errors';
-            break;
-        case JSON_ERROR_DEPTH:
-            echo 'Maximum stack depth exceeded';
-            break;
-        case JSON_ERROR_STATE_MISMATCH:
-            echo 'Underflow or the modes mismatch';
-            break;
-        case JSON_ERROR_CTRL_CHAR:
-            echo 'Unexpected control character found';
-            break;
-        case JSON_ERROR_SYNTAX:
-            echo 'Syntax error, malformed JSON';
-            break;
-        case JSON_ERROR_UTF8:
-            echo 'Malformed UTF-8 characters, possibly incorrectly encoded';
-            break;
-        default:
-            echo 'Unknown error';
-            break;
-    }    
-    $strRet = "Total: " . count($vendaPost);
+    $vendaData = json_decode($vendaPost, true); //getPost();
+    $strRet = "Total: " . count($vendaData);
+    $strRet .= " - IsArray: " . is_array($vendaData);
     echo $strRet;
-var_dump($vendaPost);
-    // $retVenda = gravarVenda($vendaPost);
+
+    // $retVenda = gravarVenda($vendaData);
     // echo json_encode($retVenda, JSON_UNESCAPED_SLASHES|JSON_NUMERIC_CHECK);
 
     
