ca28f9c5a50a5a228d1ff2aa29f837e7f9105534
aluizio-jr <aluizio.fg@gmail.com>
2022-04-07 09:33:17 -0300

    venda gravar itens

--- venda_gravar_itens.php
+++ venda_gravar_itens.php
@@ -10,14 +10,17 @@
 
                 $vendaItemID++;
 
+                $vigenciaIni = $vendaItens[$i]['item_vigencia_inicio'] ? date ('Y-m-d', strtotime($vendaItens[$i]['item_vigencia_inicio'])) : false;
+                $vigenciaFim = $vendaItens[$i]['item_vigencia_fim'] ? date ('Y-m-d', strtotime($vendaItens[$i]['item_vigencia_fim'])) : false;
+
                 $arrCampos = [
                     "lo_id_venda" =>  $vendaId,
                     "lo_id_venda_item" => $vendaItemID,
                     "lo_id_item" => $vendaItens[$i]['id_item'],
                     "lo_id_combo_vigencia" => $vendaItens[$i]['id_combo_vigencia'] ?: false,
                     "lo_id_live_turma" => $vendaItens[$i]['id_live_turma'] ?: false,
-                    "lo_item_vigencia_inicio" => $vendaItens[$i]['item_vigencia_inicio'] ?: false,
-                    "lo_item_vigencia_fim" => $vendaItens[$i]['item_vigencia_fim'] ?: false,
+                    "lo_item_vigencia_inicio" => $vigenciaIni,
+                    "lo_item_vigencia_fim" => $vigenciaFim,
                     "lo_item_quantidade" => $vendaItens[$i]['item_quantidade'] ?: false,
                     "lo_item_valor" => str_replace(',', '.', $vendaItens[$i]['item_valor']),
                     "lo_id_cupom" => $vendaItens[$i]['id_cupom'] ?: false,
